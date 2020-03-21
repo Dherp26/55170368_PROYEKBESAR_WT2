cedb1166c1fe582c245611045f143a3dc857e88a
unknown <spputra26@yahoo.com>
2020-03-21 11:06:13 +0700

    UPDATE 1

--- src/app/book.Data.ts
+++ src/app/book.Data.ts
@@ -4,36 +4,41 @@ export const BOOK: Array<Book> = [
   {
     id: 1,
     name: "Harry Potter",
-    price: 3000,
+    price: 83000,
 
   },
   {
     id: 2,
     name: "Monster Inc",
-    price: 3000,
- 
+    price: 73000,
+
   },
   {
     id: 3,
     name: "Venezule",
-    price: 3000,
+    price: 63000,
 
   },
   {
     id: 3,
     name: "Doraemon",
-    price: 3000,
+    price: 23000,
 
   },
   {
     id: 4,
     name: "Yoyo Mama",
-    price: 3000,
+    price: 33000,
 
   },
   {
     id: 5,
     name: "The Ducking",
-    price: 3000,
+    price: 53000,
+  },
+  {
+    id: 6,
+    name: "The Ilenna Tan",
+    price: 23000,
   },
 ];
